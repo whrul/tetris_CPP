89e3bf293a8eb9cfbd5c81d52b59b7d8bc6981e2
hwalerij <valeragrul@gmail.com>
2019-08-31 15:12:50 +0300

    Drawing field

--- game.cpp
+++ game.cpp
@@ -1,8 +1,9 @@
 #include "game.hpp"
 
 namespace gamestuff {
-    Game::Game(void) : scores(0), window(sf::VideoMode(FieldSize::WIDTH, FieldSize::HEIGHT), "Tetris") {
-
+    Game::Game(void) : scores(0), 
+                       window(sf::VideoMode(FieldSize::WIDTH + FieldSize::MARGIN + FieldSize::MARGIN_RIGHT, FieldSize::HEIGHT + 2 * FieldSize::MARGIN), "Tetris") {
+        this->createField();
     }
     Game::~Game() {
         
@@ -15,6 +16,36 @@ namespace gamestuff {
                     window.close();
                 }
             }
+            this->redrawAndShow();
+        }
+    }
+    void Game::createField(void) {
+        unsigned int cellsInCol = FieldSize::HEIGHT / FieldSize::CELL_SIZE;
+        unsigned int cellsInRow = FieldSize::WIDTH / FieldSize::CELL_SIZE;
+        for (unsigned int i = 0; i < cellsInCol; ++i) {
+            (this->field).push_back({});
+            for (unsigned int j = 0; j < cellsInRow; ++j) {
+                (this->field)[i].push_back(0);
+            }
+        }
+    }
+    void Game::redrawAndShow(void) {
+        (this->window).clear(sf::Color::Black);
+        this->drawFiled();
+        (this->window).display();
+    }
+    void Game::drawFiled(void) {
+        sf::RectangleShape line;
+        line.setFillColor(sf::Color(81, 81, 81));
+        for (unsigned int i = 0; i <= FieldSize::HEIGHT / FieldSize::CELL_SIZE; ++i) {
+            line.setSize(sf::Vector2f(FieldSize::WIDTH, 1));
+            line.setPosition(sf::Vector2f(FieldSize::MARGIN, FieldSize::MARGIN + FieldSize::CELL_SIZE * i));
+            (this->window).draw(line);
+        }
+        for (unsigned int i = 0; i <= FieldSize::WIDTH / FieldSize::CELL_SIZE; ++i) {
+            line.setSize(sf::Vector2f(1, FieldSize::HEIGHT));
+            line.setPosition(sf::Vector2f(FieldSize::MARGIN + FieldSize::CELL_SIZE * i, FieldSize::MARGIN));
+            (this->window).draw(line);
         }
     }
 } //namespace gamestuff
--- game.hpp
+++ game.hpp
@@ -5,20 +5,28 @@
 
 #include <vector>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
 namespace gamestuff {
     enum FieldSize {
         WIDTH = 550,
         HEIGHT = 800,
+        CELL_SIZE = 50,
+        MARGIN = 70,
+        MARGIN_RIGHT = 300,
     };
     class Game {
         unsigned int scores;
-        std::vector<unsigned char> field;
-        sf::Window window;
+        std::vector<std::vector<unsigned char>> field;
+        sf::RenderWindow window;
       public:
         Game(void);
         ~Game();
         void startGame(void);
+        void createField(void);
+        void redrawAndShow(void);
+        void drawFiled(void);
     };
 } //namespace gamestuff
 
